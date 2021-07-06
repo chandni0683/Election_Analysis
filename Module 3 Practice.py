f36eafb004110eabd2987bd3431356e8f16006d7
chandni0683 <chandni0683@gmail.com>
2021-07-05 22:23:16 -0500

    Adding the election audit code.

---
+++ Module 3 Practice.py
@@ -0,0 +1,35 @@
+type (3)
+print (type (3))
+type (2019)
+print (type(2019))
+ballots = 1,341
+type (ballots)
+print (type (ballots))
+type (73.81)
+print (type(73.81))
+type ("Hello World")
+print (type ("Hello World"))
+type (True)
+print (type(True))
+help ("keywords")
+print (help ("Keywords"))
+print (5+2*3)
+print (8 // 5 - 3)
+print(8 + 22 * 2 - 4)
+print (16 - 3 / 2 + 7 - 1)
+print(3 ** 3 % 5)
+print (5 + 9 * 3 / 2 - 4)
+print ((5 + 2) * 3)
+print ((8 // 5) - 3)
+print (8 + (22 * (2 - 4)))
+print (16 - 3 / (2 + 7) - 1)
+print (3 ** (3 % 5))
+print (5 + (9 * 3 / 2 - 4))
+print (5 + (9 * 3 / (2 - 4)))
+
+
+
+
+
+
+
